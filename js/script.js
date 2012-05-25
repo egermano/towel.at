32c9e8fb41c7fbd3c22836f61eff0e56a1f948cf
Bruno Germano <bruno.germano@gmail.com>
2012-05-25 15:32:47 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+/* Author: Bruno Germano
+   Project: towel.at
+*/
+
+var Towel = {
+	init: function(){
+
+	},
+	pages : {
+		home : function(){
+
+		},
+		towel : function(){
+
+		}
+	}
+}
+
+
+$('document').ready(function(){
+	console.log('ready');
+});
+
+
+
