f7e30d592d0269ab708f396df852de0d1270c775
Cesar Canassa <cesar.canassa@gmail.com>
2012-05-26 02:32:53 -0300

    Bugfix: permanet links are working now

--- js/script.js
+++ js/script.js
@@ -5,20 +5,27 @@
 var Towel = {
 	init: function(){
 		if(!$('#about')[0]){
+			// pre load da textura
+			var img = $('<img/>').attr('src', '/img/cloth.jpg').load();
+
 			if('/' != location.pathname && !('about/' == location.pathname )) {
 				if(!Towel.testUrl(location.pathname))
 					window.open('http://towel.at/404', '_self');
-				this.towel(location.pathname.substr(1));
+
+				CL.setup(function () {
+					alert("PANIC! you don't have webGL");
+				}, function () {
+					Towel.towel(location.pathname.substr(1));
+					CL.start();
+				});
 			}
-			this.form();
+			else {
+				CL.setup(function () {
+					alert("PANIC! you don't have webGL");
+				}, this.form);
+			}
+
 		}
-
-		CL.setup(function () {
-			alert("PANIC! you don't have webGL");
-		}, function () {});
-
-		// pre load da textura
-		var img = $('<img/>').attr('src', '/img/cloth.jpg').load();
 	},
 	form: function(){
 		$('#urlForm').click('submit', function(e){
@@ -29,6 +36,9 @@ var Towel = {
 
 			history.pushState({teste:true}, 'towel.at', '/'+url);
 			Towel.towel(url);
+
+			// monta a toalha
+			CL.start();
 
 			_gaq.push(['_trackEvent', 'Towel', 'GO', url]);
 
@@ -57,8 +67,7 @@ var Towel = {
 			});
 		});
 
-		// monta a toalha
-		CL.start();
+
 	},
 	testUrl : function(url){
 		var reTest = new RegExp(/((?:http|https):\/\/[a-z0-9\/\?=_#&%~-]+(\.[a-z0-9\/\?=_#&%~-]+)+)|(www(\.[a-z0-9\/\?=_#&%~-]+){2,})/);
